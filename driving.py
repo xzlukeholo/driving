94ae39ef05a8d987dcb4f860873a99c45fba1634
holoIA <xzlukeholo@gmail.com>
2020-10-18 16:41:07 +0800

    Add Japan

--- driving.py
+++ driving.py
@@ -2,17 +2,35 @@ name = input('請輸入您的名子:')
 gender = input('請輸入性別(男或女或其他):')
 country = input('請輸入您的國家:')
 age = input('請輸入您的年齡:')
-age = int(age)
 
 if gender == '男':
 	gender = '帥哥'
 elif gender == '女':
-	gender = '小姊姊'	
+	gender = '小姐'	
 else:
-	gender = '' 	
+	gender = ''
+
+age = int(age)
 
 if country == '台灣':
 	if age >= 18 and age <= 70:
-		print(name+gender,  '可以去考取汽車，摩托車等駕照囉!')
+		print(name+gender, '可以去考汽車駕照囉!')
 	else:
-		print(name+gender,  '還不能考駕照喔!')
+		print(name+gender, '還不能考駕照喔!')
+elif country == '美國':
+	if age >= 16:
+		print(name+gender, '可以去考汽車駕照囉!')
+	else:
+		print(name+gender, '還不能考駕照喔!')
+elif country == '日本':
+	if age >= 18:
+		print(name+gender, '可以去考汽車駕照囉!')
+	else:
+		print(name+gender, '還不能考駕照喔!')
+elif country == '中國':
+	if age >= 18:
+		print(name+gender, 'owo')
+	else:
+		print(name+gender, '0w0')
+else:
+	print(name+gender, '抱歉喔!我們目前只能輸入台灣、美國、日本喔')
